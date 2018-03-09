178b5899b622c374b5c22657ebfda38a308cafe5
Matt Cuneo <m@jcuneo.com>
2018-03-09 21:41:06 +1100

    still working on heroku

--- src/server/index.js
+++ src/server/index.js
@@ -3,14 +3,19 @@ import app from './server';
 
 const { NODE_ENV, PORT, } = process.env;
 const isDev = NODE_ENV !== 'production';
-console.log(NODE_ENV, PORT); //eslint-disable-line
+console.log(NODE_ENV, PORT, process.env); //eslint-disable-line
 
 const SERVER_PORT = PORT || 8080;
+const SERVER_HOST = isDev ? 'localhost' : '0.0.0.0';
 
 const server = Server( app );
 let currentApp = app;
 
-server.listen( SERVER_PORT );
+server.listen(
+  SERVER_PORT,
+  SERVER_HOST,
+  () => console.log(`listening on port ${SERVER_PORT}`) //eslint-disable-line
+);
 
 if ( isDev ) {
   if ( module.hot ) {
