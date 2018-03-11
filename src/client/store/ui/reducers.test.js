ccba7ad1bd9521de289daa4c3619336024a0ac56
Matt Cuneo <m@jcuneo.com>
2018-03-11 22:51:10 +1100

    fixed a test

--- src/client/store/ui/reducers.test.js
+++ src/client/store/ui/reducers.test.js
@@ -4,10 +4,21 @@ import { ui, uiInitialState, } from '.';
 describe(
   'ui', () => {
     it(
-      'should handle toggling the open keys', () => {
+      'should handle toggling the collapse state of the sidebar', () => {
+        const expectedAction = { isSidebarCollapsed : false,
+                                 openKeys           : [], };
+
+        expect( ui(
+          uiInitialState, toggleSidebar()
+        ) ).toEqual( expectedAction );
+      }
+    );
+
+    it(
+      'should add an open key when a user toggles a menu item open on the sidebar', () => {
         const key = '1';
         const expectedAction = { isSidebarCollapsed : true,
-                                 openKeys           : [ '1', ], };
+                                 openKeys           : [ key, ], };
 
         expect( ui(
           uiInitialState, toggleKey( key )
@@ -16,12 +27,15 @@ describe(
     );
 
     it(
-      'should handle toggling the sidebar', () => {
-        const expectedAction = { isSidebarCollapsed : false,
+      'should remove an open key when a user toggles a menu item closed on the sidebar', () => {
+        const key = '1';
+        const expectedAction = { isSidebarCollapsed : true,
                                  openKeys           : [], };
+        const initialState = { isSidebarCollapsed : true,
+                               openKeys           : [ '1', ], };
 
         expect( ui(
-          uiInitialState, toggleSidebar()
+          initialState, toggleKey( key )
         ) ).toEqual( expectedAction );
       }
     );
