47905d6d31ad775def78eb695df9804fc9abf224
rabidpug <m@jcuneo.com>
2018-03-14 15:17:09 +1100

    Added more container tests

---
+++ src/client/containers/TodoTopBar/index.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import TodoTopBar from '.';
+import actionMenu from '../../routes/actionMenu';
+import configureStore from 'redux-mock-store';
+import { shallow, } from 'enzyme';
+import { uiInitialState, } from '../../store/ui';
+
+const mockStore = configureStore();
+
+describe(
+  'HomeTopBar Container', () => {
+    let store,
+      wrapper;
+    const ownProps = { route: actionMenu[0], };
+
+    beforeEach( () => {
+      store = mockStore( { ui: uiInitialState, } );
+
+      store.dispatch = jest.fn();
+
+      wrapper = shallow( <TodoTopBar
+        store={ store }
+        { ...ownProps } /> );
+    } );
+
+    it(
+      'maps state and dispatch to props', () => {
+        const expectedAction = expect.objectContaining( { actionMenuItems : ownProps.route.actionMenuItems,
+                                                          toggleSideBar   : expect.any( Function ), } );
+
+        expect( wrapper.props() ).toEqual( expectedAction );
+      }
+    );
+
+    describe(
+      'toggleSideBar', () => {
+        it(
+          'maps TOGGLE_SIDEBAR to dispatch action', () => {
+            const expectedAction = { type: 'TOGGLE_SIDEBAR', };
+
+            wrapper.props().toggleSideBar();
+
+            expect( store.dispatch ).toHaveBeenCalledWith( expectedAction );
+          }
+        );
+      }
+    );
+  }
+);
