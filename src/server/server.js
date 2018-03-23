3189ec676e4fc24ce2e3bd8812ebd44d75714deb
Matt Cuneo <m@jcuneo.com>
2018-03-23 13:44:33 +1100

    trying to get favicon to work

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,11 @@
 import config from '../../webpack/webpack.config.client';
 import express from 'express';
+import favicon from 'serve-favicon';
 import { helloEndpointRoute, } from 'Shared/routes';
 import path from 'path';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-
 const { NODE_ENV, } = process.env;
 const app = express(),
   isDev = NODE_ENV !== 'production';
@@ -13,7 +13,7 @@ const app = express(),
 if ( isDev ) {
   const compiler = webpack( config );
 
-  const DEV_HTML_FILE = path.resolve(
+  const HTML_FILE = path.resolve(
     compiler.outputPath, 'index.html'
   );
 
@@ -36,7 +36,7 @@ if ( isDev ) {
       req, res, next
     ) => {
       compiler.outputFileSystem.readFile(
-        DEV_HTML_FILE, (
+        HTML_FILE, (
           err, result
         ) => {
           if ( err ) return next( err );
@@ -53,9 +53,13 @@ if ( isDev ) {
     }
   );
 } else {
-  const HTML = path.resolve(
+  const HTML_FILE = path.resolve(
     __dirname, 'index.html'
   );
+
+  app.use( favicon( path.resolve(
+    __dirname, 'favicon.ico'
+  ) ) );
 
   app.use( express.static( __dirname ) );
 
@@ -75,7 +79,7 @@ if ( isDev ) {
         'content-type', 'text/html'
       );
 
-      res.sendFile( HTML );
+      res.sendFile( HTML_FILE );
     }
   );
 }
