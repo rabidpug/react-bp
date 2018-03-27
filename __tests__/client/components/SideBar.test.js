c663effc765b44592da029ce228e744e9c8968fe
Matt Cuneo <m@jcuneo.com>
2018-03-27 16:49:12 +1100

    Added event trigger tests for SideBar component

--- __tests__/client/components/SideBar.test.js
+++ __tests__/client/components/SideBar.test.js
@@ -6,15 +6,63 @@ import { shallowToJson, } from 'enzyme-to-json';
 
 describe(
   'SideBar', () => {
-    it(
-      'Should render the SideBar', () => {
-        const [ { menuItems, }, ] = navMenu;
-        const wrapper = shallow( <SideBar
-          currentPath='/'
-          menuItems={ menuItems }
-          openKeys={ [] } /> );
+    const goToPath = jest.fn();
+    const toggleKey = jest.fn();
+    const toggleSideBar = jest.fn();
+    const [ { menuItems, }, ] = navMenu;
+    const item = { item: { props: { path: '/', }, }, };
+    const wrapper = shallow( <SideBar
+      currentPath='/'
+      goToPath={ goToPath }
+      isSidebarCollapsed={ false }
+      menuItems={ menuItems }
+      openKeys={ [] }
+      toggleKey={ toggleKey }
+      toggleSideBar={ toggleSideBar }
+    /> );
 
+    it(
+      'Should render the SideBar with a menu', () => {
         expect( shallowToJson( wrapper ) ).toMatchSnapshot();
+      }
+    );
+
+    it(
+      'should call goToPath with items path when menu clicked', () => {
+        wrapper.find( 'Menu' ).simulate(
+          'click', item
+        );
+
+        expect( goToPath ).toBeCalledWith( '/' );
+      }
+    );
+
+    it(
+      'should toggle submenu expansion when submenu item clicked', () => {
+        wrapper
+          .find( 'SubMenu' )
+          .props()
+          .onTitleClick( { key: 'sub1', } );
+
+        expect( toggleKey ).toBeCalledWith( 'sub1' );
+      }
+    );
+
+    it(
+      'should call toggleSideBar when the mouse leaves and the sidebar is not collapsed', () => {
+        wrapper.find( 'Sider' ).simulate( 'mouseleave' );
+
+        expect( toggleSideBar ).toBeCalled();
+      }
+    );
+
+    it(
+      'should call toggleSideBar when the mouse enters and the sidebar is collapsed', () => {
+        wrapper.setProps( { isSidebarCollapsed: true, } );
+
+        wrapper.find( 'Sider' ).simulate( 'mouseenter' );
+
+        expect( toggleSideBar ).toBeCalled();
       }
     );
   }
