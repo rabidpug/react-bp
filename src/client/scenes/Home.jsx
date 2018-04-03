96d7e2c053f76b9549220e17eab7fc6779f2763e
Matt Cuneo <m@jcuneo.com>
2018-04-03 17:48:37 +1000

    extra welcome message

--- src/client/scenes/Home.jsx
+++ src/client/scenes/Home.jsx
@@ -1,6 +1,10 @@
 import FadeWrap from 'Animations/FadeWrap';
 import React from 'react';
 
-const Home = FadeWrap( ( { style, } ) => <div style={ { ...style, } }>Welcome to React ENFRAMR!</div> );
+const Home = FadeWrap( ( { style, } ) => (
+  <div style={ { ...style, } }>
+    Welcome to React ENFRAMR! A React Boilerplate with lots of stuff built in to save me time.
+  </div>
+) );
 
 export default Home;
