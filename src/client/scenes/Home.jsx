f65cd4a220ed3fa9d8b35767083c8281a932131e
Matt Cuneo <m@jcuneo.com>
2018-04-04 13:05:58 +1000

    test push

--- src/client/scenes/Home.jsx
+++ src/client/scenes/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Home = FadeWrap( ( { style, } ) => (
   <div style={ { ...style, } }>
-    Welcome to React ENFRAMR! A React Boilerplate with lots of stuff built in to save me time.
+    Welcome to React ENFRAMR! A React Boilerplate with lots of stuff built in to save me time. Lots of things, indeed
   </div>
 ) );
 
