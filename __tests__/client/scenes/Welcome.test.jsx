bc3848db7d6dae6bb0c9f535031de18a724c915c
Matt Cuneo <m@jcuneo.com>
2018-04-11 11:15:18 +1000

    fixed snapshot

--- __tests__/client/scenes/Welcome.test.jsx
+++ __tests__/client/scenes/Welcome.test.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Welcome from 'Scenes/Welcome';
+import configureStore from 'redux-mock-store';
 import { shallow, } from 'enzyme';
 import { shallowToJson, } from 'enzyme-to-json';
 
+const mockStore = configureStore();
+
 describe(
   'TopBar', () => {
+    const store = mockStore( {} );
+
+    store.dispatch = jest.fn();
+
     it(
-      'Should render the TopBar', () => {
-        const wrapper = shallow( <Welcome /> );
+      'Should render the Welcome page', () => {
+        const wrapper = shallow( <Welcome store={ store } /> );
 
         expect( shallowToJson( wrapper ) ).toMatchSnapshot();
       }
--- src/client/scenes/Welcome.jsx
+++ src/client/scenes/Welcome.jsx
@@ -1,4 +1,3 @@
-import FadeWrap from 'Animations/FadeWrap';
 import HelloMessage from 'Components/Message/HelloMessage';
 
 const Welcome = HelloMessage;
