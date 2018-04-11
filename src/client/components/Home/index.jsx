caea577f4c982ff21c44075220f867b2e235aafa
Matt Cuneo <m@jcuneo.com>
2018-04-12 01:33:03 +1000

    updated home layout

--- src/client/components/Home/index.jsx
+++ src/client/components/Home/index.jsx
@@ -3,7 +3,12 @@ import gStyles from 'Styles/global';
 
 const Home = () => (
   <div className={ gStyles.cardStyle }>
-    Welcome to React ENFRAMR! A Full Stack React Boilerplate with lots of stuff built in to save me time.
+    <div className={ gStyles.cardHeader }>
+      <h1>Welcome to React ENFRAMR!</h1>
+    </div>
+    <div className={ gStyles.cardTop }>
+      <p> A Full Stack React Boilerplate with lots of stuff built in to save me time.</p>
+    </div>
   </div>
 );
 
