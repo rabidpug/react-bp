135528bdc78f1621a381615c8daf98656bc353d5
Matt Cuneo <m@jcuneo.com>
2018-04-13 12:33:11 +1000

    fixed more tests

--- __tests__/client/store/greeter/selectors.test.js
+++ __tests__/client/store/greeter/selectors.test.js
@@ -1,46 +1,21 @@
-import {
-  getGreeterIsLoading,
-  getGreeterLabel,
-  getGreeterMessage,
-} from 'Store/greeter/selectors';
+import { getGreeterIsLoading, getGreeterMessages, } from 'Store/greeter/selectors';
 
 import { greeterInitialState, } from 'Store/greeter';
 
-describe(
-  'getGreeterLabel', () => {
-    it(
-      'should get the Greeter Label', () => {
-        const state = { greeter: greeterInitialState, };
-        const expectedAction = greeterInitialState.label;
+describe( 'getGreeterIsLoading', () => {
+  it( 'should get the Greeter Loading state', () => {
+    const state = { greeter: greeterInitialState, };
+    const expectedAction = greeterInitialState.isLoading;
 
-        expect( getGreeterLabel( state ) ).toEqual( expectedAction );
-      }
-    );
-  }
-);
+    expect( getGreeterIsLoading( state ) ).toEqual( expectedAction );
+  } );
+} );
 
-describe(
-  'getGreeterIsLoading', () => {
-    it(
-      'should get the Greeter Loading state', () => {
-        const state = { greeter: greeterInitialState, };
-        const expectedAction = greeterInitialState.isLoading;
+describe( 'getGreeterMessages', () => {
+  it( 'should get the greeter message', () => {
+    const state = { greeter: greeterInitialState, };
+    const expectedAction = greeterInitialState.messages;
 
-        expect( getGreeterIsLoading( state ) ).toEqual( expectedAction );
-      }
-    );
-  }
-);
-
-describe(
-  'getGreeterMessage', () => {
-    it(
-      'should get the greeter message', () => {
-        const state = { greeter: greeterInitialState, };
-        const expectedAction = greeterInitialState.message;
-
-        expect( getGreeterMessage( state ) ).toEqual( expectedAction );
-      }
-    );
-  }
-);
+    expect( getGreeterMessages( state ) ).toEqual( expectedAction );
+  } );
+} );
