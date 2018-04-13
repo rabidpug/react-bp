98bf1d20451110318cd1cecb73f958e4aaee166e
Matt Cuneo <m@jcuneo.com>
2018-04-13 12:31:53 +1000

    fixed tests

--- __tests__/client/store/greeter/reducers.test.js
+++ __tests__/client/store/greeter/reducers.test.js
@@ -1,53 +1,32 @@
 import { greeter, greeterInitialState, } from 'Store/greeter';
-import {
-  sayHelloFailure,
-  sayHelloRequest,
-  sayHelloSuccess,
-} from 'Store/greeter/actions';
+import { sayHelloFailure, sayHelloRequest, sayHelloSuccess, } from 'Store/greeter/actions';
 
-describe(
-  'greeter', () => {
-    it(
-      'should handle a hello request', () => {
-        const expectedAction = {
-          isLoading : true,
-          label     : 'Loading...',
-          message   : 'Loading...',
-        };
+describe( 'greeter', () => {
+  it( 'should handle a hello request', () => {
+    const expectedAction = {
+      isLoading : true,
+      messages  : [],
+    };
 
-        expect( greeter(
-          greeterInitialState, sayHelloRequest()
-        ) ).toEqual( expectedAction );
-      }
-    );
+    expect( greeter( greeterInitialState, sayHelloRequest() ) ).toEqual( expectedAction );
+  } );
 
-    it(
-      'should handle a hello failure', () => {
-        const expectedAction = {
-          isLoading : false,
-          label     : 'Try saying hi again!',
-          message   : 'No message received, please check your connection and try again',
-        };
+  it( 'should handle a hello failure', () => {
+    const expectedAction = {
+      isLoading : false,
+      messages  : [ 'An error has occurred!', ],
+    };
 
-        expect( greeter(
-          greeterInitialState, sayHelloFailure()
-        ) ).toEqual( expectedAction );
-      }
-    );
+    expect( greeter( greeterInitialState, sayHelloFailure() ) ).toEqual( expectedAction );
+  } );
 
-    it(
-      'should handle a hello success', () => {
-        const message = 'Hai :3';
-        const expectedAction = {
-          isLoading : false,
-          label     : 'Say it again!',
-          message,
-        };
+  it( 'should handle a hello success', () => {
+    const message = 'Hai :3';
+    const expectedAction = {
+      isLoading : false,
+      messages  : [ message, ],
+    };
 
-        expect( greeter(
-          greeterInitialState, sayHelloSuccess( message )
-        ) ).toEqual( expectedAction );
-      }
-    );
-  }
-);
+    expect( greeter( greeterInitialState, sayHelloSuccess( message ) ) ).toEqual( expectedAction );
+  } );
+} );
